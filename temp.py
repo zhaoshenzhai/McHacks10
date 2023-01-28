e2a8a952195207fb8900d974337e05f5ac997a6f
zhaoshenzhai <zhaoshen.zhai@gmail.com>
2023-01-28 14:00:42 -0500

    Works

--- temp.py
+++ temp.py
@@ -1,13 +1,21 @@
-from xgoogle.search import GoogleSearch, SearchError
+import requests
+from bs4 import BeautifulSoup
+import re
 
-try:
-    gs = GoogleSearch("quick and dirty")
-    gs.results_per_page = 50
-    results = gs.get_results()
-    for res in results:
-        print(res.title.encode("utf8"))
-        print(res.desc.encode("utf8"))
-        print(res.url.encode("utf8"))
-        print
-except (SearchError, e):
-    print("hi")
+query = "data science"
+search = query.replace(' ', '+')
+results = 10
+url = (f"https://www.google.com/search?q={search}&num={results}")
+
+requests_results = requests.get(url)
+soup_link = BeautifulSoup(requests_results.content, "html.parser")
+links = soup_link.find_all("a")
+
+for link in links:
+    link_href = link.get('href')
+    if "url?q=" in link_href and not "webcache" in link_href:
+      title = link.find_all('h3')
+      if len(title) > 0:
+          print(link.get('href').split("?q=")[1].split("&sa=U")[0])
+          print(title[0].getText())
+          print("------")
